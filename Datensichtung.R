e043a61785fa48443c260675d0f14fbefb016fb6
JonathanSenn <jonathan.senn88@gmail.com>
2017-05-02 10:34:29 +0200

    deleted

--- Datensichtung.R
+++
@@ -1,3 +0,0 @@
-testfile <- "https://opentransportdata.swiss/dataset/695c7af6-d486-4cde-9bf0-a92fdd581a4e/resource/ab66fad3-6fed-46b6-80fa-314168812b15/download/bahnhof.csv"
-
-daten <- read.csv(testfile, encoding = "utf-8")
