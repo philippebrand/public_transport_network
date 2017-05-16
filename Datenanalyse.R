90e05fecbae95daa0499dcc5981391e79d65fc93
JonathanSenn <jonathan.senn88@gmail.com>
2017-05-16 08:43:03 +0200

    daten einlesen

--- Datenanalyse.R
+++ Datenanalyse.R
@@ -10,6 +10,8 @@ library(plyr)      # install.packages("plyr")
 library(sp)        # install.packages("sp")
 library(leaflet)   # install.packages("leaflet")
 library(spatgraphs)# install.packages("spatgraphs")
+library(ggplot2)
+library(ggmap)
 
 
 dataFolder <- "data"
@@ -21,6 +23,23 @@ dataFolder <- "data"
 istdaten <- read.csv(file.path(dataFolder,"istDaten","2017-05-08istdaten.csv"), 
                      encoding = "UTF-8",stringsAsFactors = FALSE, sep = ";")
 
-koordinaten <- read.csv(file.path(dataFolder,"fp2017jahresfahrplan","BFKOORD"), 
-                     encoding = "UTF-8",stringsAsFactors = FALSE, sep = " ")
-read.fwf(ff, widths = c(1,-2,3))
+koordinaten <- read.csv(file.path(dataFolder,"stationsliste","bfkoordgeo.csv"), 
+                     encoding = "UTF-8",stringsAsFactors = FALSE, sep = ",")
+
+
+daten <- merge.data.frame(istdaten, koordinaten, by.x = "BPUIC", by.y = "StationID", all.x = TRUE, sort = FALSE)
+
+#Rasterkarte als Hintergrund
+CH <- get_map(location = c(median(koordinaten$Longitude, na.rm = TRUE), median(koordinaten$Latitude, na.rm = TRUE)), zoom = 7, source = 'google', color = 'bw')
+map <- ggmap(CH)
+
+#Small Multiples nach Kategorie cat
+map <- map + geom_point(data = koordinaten, aes(Longitude, Latitude, color = Height)) + theme_minimal()
+map
+
+ggsave("plot.png", width = 15, height = 15)
+
+
+
+
+
